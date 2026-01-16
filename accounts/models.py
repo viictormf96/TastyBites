0c133aae29cb940ab4d9c91936131d4f6d318639
Victor Muntane Fuentes <vmuntane96it@gmail.com>
2026-01-16 18:56:49 +0100

    move recipe model follower to accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -10,5 +10,13 @@ class CustomUser(AbstractUser):
     class Meta:
         db_table = "users"
 
-
+# Followers model
+class Follower(models.Model):
+    follower = models.ForeignKey(CustomUser, related_name="following", on_delete=models.CASCADE)
+    followee = models.ForeignKey(CustomUser, related_name="followers", on_delete=models.CASCADE)
+    class Meta:
+        db_table = "followers"
+        unique_together = ("follower", "followee")
+    def __str__(self):
+        return f"{self.follower.username} sigue {self.followee.username}"
 
--- recipes/models.py
+++ recipes/models.py
@@ -102,14 +102,5 @@ class Favorite(models.Model):
     def __str__(self):
         return f"{self.user.username} le gusta {self.recipe.name}"
 
-# Followers model
-class Follower(models.Model):
-    follower = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
-    followee = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
-    class Meta:
-        db_table = "followers"
-        unique_together = ("follower", "followee")
-    def __str__(self):
-        return f"{self.follower.username} sigue {self.followee.username}"
     
 
