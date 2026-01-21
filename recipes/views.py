61e271a92e592cdc9c120284f3a9757a8d9a600c
Victor Muntane Fuentes <vmuntane96it@gmail.com>
2026-01-21 20:02:15 +0100

    Feat: implement django templates in categories.html

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,13 @@ from django.db.models import Count
 from .models import Recipe, Category
 
 
-class CategoriesDashboardView(TemplateView):
+class CategoriesDashboardView(ListView):
+    model = Category
+    context_object_name = "categories_list"
     template_name = "categories/categories.html"
+
+    def get_queryset(self):
+        return super().get_queryset().annotate(
+            total_favorites = Count("recipe__favorites", distinct=True),
+            total_recipes = Count("recipe", distinct=True)
+        ).order_by("-total_recipes")
